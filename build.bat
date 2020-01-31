b57e43fef9285d4509d7eb7b8e575642889460b5
myusufcil <45545069+myusufcil@users.noreply.github.com>
2020-01-31 14:46:58 +0300

    jenkins

--- build.bat
+++ build.bat
@@ -12,8 +12,8 @@ echo "Java version:"
 java -version
 
 mkdir "${ANDROID_HOME}/licenses" || true
-echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > $ANDROID_HOME/licenses/android-sdk-license
-echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > $ANDROID_HOME/licenses/android-sdk-license
+echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_HOME/licenses/android-sdk-license"
+echo "d56f5187479451eabf01fb78af6dfcb131a6481e" > "$ANDROID_HOME/licenses/android-sdk-license"
 echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_HOME/licenses/android-sdk-license"
 
 git log --pretty=format:'+ %s' -n 14 | grep -v "Merge\|release\|Update circle\|Update build" > change.log
